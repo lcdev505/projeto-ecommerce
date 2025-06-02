9a89e227d0da5a8297b58c37bf73ab025d73c0e7
lcdev505 <lcsg2103@gmail.com>
2025-06-02 11:14:16 -0300

    Delete backend directory

--- backend/src/routes/paymentRoutes.ts
+++
@@ -1,9 +0,0 @@
-// backend/src/routes/paymentRoutes.ts
-import express from "express";
-import { handleWebhook } from "../controllers/paymentController";
-
-const router = express.Router();
-
-router.post("/", handleWebhook);
-
-export default router;
--- backend/src/utils/asyncHandler.ts
+++
@@ -1,8 +0,0 @@
-// backend/src/utils/asyncHandler.ts
-import { Request, Response, NextFunction } from "express";
-
-export const asyncHandler =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
